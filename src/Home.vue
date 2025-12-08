f1213a981de4ad56223b96019bd8c867d385cf72
Md Sakibul Hussain (GROUTINE) <sakal@groutine.pc>
2025-12-08 16:47:59 +0530

    My BAd

--- src/Home.vue
+++ src/Home.vue
@@ -5,6 +5,20 @@
       <div class="ad-placeholder">Ad Space: 970x90</div>
     </div>
 
+    <div style="position: absolute; z-index: 99999">
+      <input autocomplete="off" type="checkbox" id="aadsstickymix1gobh" hidden />
+      <div style="padding-top: auto; padding-bottom: 0;">
+        <div style="width:100%;height:auto;position:fixed;text-align:center;font-size:0;top:0;left:0;right:0;margin:auto">
+          <label for="aadsstickymix1gobh" style="top: 50%;transform: translateY(-50%);right:24px;; position: absolute;border-radius: 4px; background: rgba(248, 248, 249, 0.70); padding: 4px;z-index: 99999;cursor:pointer">
+            <svg fill="#000000" height="16px" width="16px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 490 490">
+              <polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 489.292,32.337 "/>
+            </svg>
+          </label>
+          <div id="frame" style="width: 100%;margin: auto;position: relative; z-index: 99998;"><iframe data-aa=2419949 src="//acceptable.a-ads.com/2419949/?size=Adaptive" style='border:0; padding:0; width:70%; height:auto; overflow:hidden; margin: auto'></iframe></div>
+        </div>
+    </div>
+  </div>
+
     <section class="section">
       <h2 class="section-title">✨ Latest Blogs</h2>
 
@@ -172,6 +186,11 @@ export default {
 <style>
 /* 1. Base & Layout
 */
+
+      #aadsstickymix1gobh:checked + div {
+        display: none;
+      }
+
 body {
   margin: 0;
   background-color: #0d1117; /* Dark background */
